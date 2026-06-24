124caa7a290f07a751fc13a390805932d6145b28
Astika Sinha <139857966+astikasinha@users.noreply.github.com>
2026-06-24 10:04:20 +0530

    changed otpcontroller line 33

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -30,12 +30,14 @@ const sendOtp = async (req, res) => {
         });
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
